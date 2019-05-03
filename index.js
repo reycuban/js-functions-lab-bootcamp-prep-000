7435f6d27ffa82a959d5afec3e35e0c4c4bff21b
Reinier Blanco <reinierblanco10@gmail.com>
2019-05-03 18:55:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function happyHolydayTo(holyday, name){
 function holydayCountdown(days, holyday) {
   return `It's ${days} days until ${holiday}`;
 }
+
+happyHolidaysTo("Reinier");
+happyHolydayTo("Holydays","Reinier");
+holydayCountdown("May 3","Holydays");
